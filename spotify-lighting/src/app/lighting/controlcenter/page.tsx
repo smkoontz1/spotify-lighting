6427b576e03791cec5fc2f7d65e1b288e597e48b
Steve Koontz <smkoontz1@gmail.com>
2024-11-24 13:33:57 -0600

    All the react errors are gone added light state map

--- spotify-lighting/src/app/lighting/controlcenter/page.tsx
+++ spotify-lighting/src/app/lighting/controlcenter/page.tsx
@@ -5,109 +5,109 @@ import { useLight } from '../_lib/hooks/useLight'
 import { ChangeEvent, useEffect, useState } from 'react'
 import { useLights } from '../_lib/hooks/useLights'
 
+
 export default function ControlCenter() {
   const { data } = useLights()
   const { mutate } = useLight()
-  const [lightRowElements, setLightRowElements] = useState<JSX.Element[]>([])
+
+  const [lightMap, setLightMap] = useState<Map<string, boolean>>(new Map<string, boolean>())
+
+  useEffect(() => {
+    if (data) {
+      data.forEach(l => {
+        setLightMap(lightMap.set(l.hueId, false))
+      })
+    }
+  }, [data])
 
   const handleSwitchChange = (event: ChangeEvent) => {
     const id = event.target.id
     const hueId = id.split('/')[1]
     const checked = (event.target as any).checked as boolean
 
-    console.log('Mutating')
-
+    setLightMap(lightMap.set(hueId, checked))
+    
     mutate({ id: hueId, on: checked })
   }
 
-  useEffect(() => {
-    console.log('DATA CHANGED', data)
+  const LightCard = ({ name, hueId }: { name: string, hueId: string }) => {
+    return (
+      <Card style={{ width: '18rem' }} key={`light-${hueId}`}>
+        <CardBody>
+          <CardTitle>{name}</CardTitle>
+          <Form>
+            <Form.Check
+              type='switch'
+              id={`switch/${hueId}`}
+              label='Off/On'
+              checked={lightMap.get(hueId) || false}
+              onChange={handleSwitchChange}
+            />
+          </Form>
+        </CardBody>
+      </Card>
+    )
+  }
 
+  const LightRows = () => {
+    let rowElements: JSX.Element[] = []
+    
     if (data) {
       const rowCount = data.length / 2
       const remainingLights = data.length % 2
-
+  
       let leftColumnIndex = 0
-
-      let rowElements: JSX.Element[] = []
+  
   
       for (let row = 1; row <= rowCount; row++) {
         const leftLight = data[leftColumnIndex]
         const rightLight = data[leftColumnIndex + 1]
-
+  
         const rowElement =
-          <Row>
+          <Row key={`row-${row}`}>
             <Col>
-              <Card style={{ width: '18rem' }}>
-                <CardBody>
-                  <CardTitle>{leftLight.name}</CardTitle>
-                  <Form>
-                    <Form.Check
-                      type='switch'
-                      id={`switch/${leftLight.hueId}`}
-                      label="Off/On"
-                      onChange={handleSwitchChange}
-                    />
-                  </Form>
-                </CardBody>
-              </Card>
+              <LightCard
+                name={leftLight.name}
+                hueId={leftLight.hueId}
+              />
             </Col>
             <Col>
-              <Card style={{ width: '18rem' }}>
-                <CardBody>
-                  <CardTitle>{rightLight.name}</CardTitle>
-                  <Form>
-                    <Form.Check
-                      type='switch'
-                      id={`switch/${rightLight.hueId}`}
-                      label="Off/On"
-                      onChange={handleSwitchChange}
-                    />
-                  </Form>
-                </CardBody>
-              </Card>
+              <LightCard
+                name={rightLight.name}
+                hueId={rightLight.hueId}
+              />
             </Col>
           </Row>
-
+  
         rowElements = [...rowElements, rowElement]
         leftColumnIndex += 2
       }
-
+  
       if (remainingLights > 0) {
         const lastLight = data[data.length - 1]
-
+  
         const lastRowElement =
-          <Row>
+          <Row key={'last-row'}>
             <Col>
-              <Card style={{ width: '18rem' }}>
-                <CardBody>
-                  <CardTitle>{lastLight.name}</CardTitle>
-                  <Form>
-                    <Form.Check
-                      type='switch'
-                      id={`switch/${lastLight.hueId}`}
-                      label="Off/On"
-                      onChange={handleSwitchChange}
-                    />
-                  </Form>
-                </CardBody>
-              </Card>
+              <LightCard
+                key={`light-${lastLight.hueId}`}
+                name={lastLight.name}
+                hueId={lastLight.hueId}
+              />
             </Col>
           </Row>
         
         rowElements = [...rowElements, lastRowElement]
       }
-
-      setLightRowElements(rowElements)
     }
-  }, [data])
 
-  console.log('Rendering')
+    return rowElements
+  }
 
   return(
     <>
       <Container>
-        {lightRowElements}
+        <LightRows />
       </Container>
     </>
   )
